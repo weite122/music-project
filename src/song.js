54073369ac72daac86cab7fe54b64264bb23a649
weite122 <949193531@qq.com>
2017-08-14 18:04:59 +0800

    实现播放功能

--- src/song.js
+++ src/song.js
@@ -15,7 +15,14 @@ $(function(){
             if(!object){return}
             let $p = $('<p/>')
             $p.attr('data-time',object.time).text(object.words)
-            $p.appendTo($lyric)
+            $p.appendTo($lyric.children('.lines'))
         })
     })
+
+    let audio = document.createElement('audio')
+    audio.src = "http://m10.music.126.net/20170814150932/68d2e91fba2ec79cffe12307a81bcbfc/ymusic/3971/cb87/4b28/18eab5a47394b8cf0333153e4349b7fa.mp3"
+    audio.oncanplay = function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    }
 })
