9e2301c384885adb578014956cf39e35262e6239
weite122 <949193531@qq.com>
2017-08-14 21:14:40 +0800

    暂停功能完成

--- src/song.js
+++ src/song.js
@@ -9,7 +9,7 @@ $(function(){
                 return {time: matches[1],words:matches[2]}
             } 
         })
-        console.log(array)
+        // console.log(array)
         let $lyric = $('.lyric')
         array.map(function(object){
             if(!object){return}
@@ -20,9 +20,17 @@ $(function(){
     })
 
     let audio = document.createElement('audio')
-    audio.src = "http://m10.music.126.net/20170814150932/68d2e91fba2ec79cffe12307a81bcbfc/ymusic/3971/cb87/4b28/18eab5a47394b8cf0333153e4349b7fa.mp3"
+    audio.src = "http://dl.stream.qqmusic.qq.com/C400001W4LsO0FHzS6.m4a?vkey=86F51F626E69FB4C19A767CB38CDFC11A8D17DCAE203E4B4DF4C0CAD1A1710F03130DCA30B6514B3AF6464D4E2C52951F1F3EDB89A592A60&guid=1803739836&uin=949193531&fromtag=66"
     audio.oncanplay = function(){
         audio.play()
         $('.disc-container').addClass('playing')
     }
+    $('.icon-pause').on('click',function(){
+        audio.pause()
+        $('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click',function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    })
 })
