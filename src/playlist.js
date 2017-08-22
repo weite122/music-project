4469a174b229aa265a3109a4b05dbde5a854430c
weite122 <949193531@qq.com>
2017-08-23 00:06:08 +0800

    [C]playlist.html歌单自动获取

--- src/playlist.js
+++ src/playlist.js
@@ -1,4 +1,34 @@
-$('.introductionText').click(function(){
-    $('.introductionText').toggleClass('noactive')
-    $('.introductionText').siblings().find('span').toggleClass('active')
+$(function(){
+    
+    $('.introductionText').click(function(){
+        $('.introductionText').toggleClass('noactive')
+        $('.introductionText').siblings().find('span').toggleClass('active')
+    })
+
+
+    $.get('./songs.json').then(function(response){
+        let items = response
+        items.forEach((i)=>{
+            let $li = $(`
+            <li>
+            <a  href="./song.html?id=${i.id}">
+                <span class="listNumber">${i.id}</span>
+                <div class="listInformation">
+                <h3>${i.name}</h3>
+              <p>
+              <svg class="sq">
+                <use xlink:href="#icon-SQ"></use>
+              </svg>          
+              ${i.information}</p>
+              </div>
+              <svg class="play">
+                <use xlink:href="#icon-play-circle"></use>
+              </svg>
+            </a>
+          </li>
+                 `)
+             $('#lastestMusic').append($li)
+        })
+        $('#lastestMusicLoading').remove()
+     })
 })
